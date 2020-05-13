ccf05ef04eb217b591e79b79dfe2c02a69dab24c
vh88639 <60492435+vh88639@users.noreply.github.com>
2020-05-13 09:35:57 -0700

    Create words.py

---
+++ words.py
@@ -0,0 +1,18 @@
+#You can add all the words you want to use in this list
+word_list=[
+    "expensive",
+    "auspicious",
+    "industrious",
+    "cooperative",
+    "funny",
+    "helpful",
+    "indecisive",
+    "magical",
+    "greatful",
+    "playful",
+    "hungry",
+    "unicorn",
+    "mermaid",
+    "invincible",
+    "penguin"
+]
